1fe6b728968937e1ee4e50bea00ee2f7e975e71b
arnaud cormier <arno.cormier@gmail.com>
2019-11-06 10:30:51 +0100

    made navuser without form select

--- src/Blocks/NavUser.svelte
+++ src/Blocks/NavUser.svelte
@@ -4,7 +4,27 @@
 <style>
 .navuser{
 grid-area: navuser;
+
+}
+.media {
+	padding:0.7rem;
+}
+.content {
+	padding:0.7rem;
 }
 </style>
 
-<nav class="navuser">Monica WHite</nav>
+<nav class="navuser has-background-grey">
+	<article class="media">
+		<div class="media-left">
+				<figure class="image is-48x48">
+					<img class="is-rounded" src="https://bulma.io/images/placeholders/48x48.png">
+				</figure>
+			</div>
+			<div class="media-content">
+				<div class="content">
+					<p><strong>Monica WHite</strong></p>
+				</div>
+		</div>
+	</article>
+</nav>
